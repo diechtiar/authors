e99fc8aaf04d5fda75d75257828aff10729a3343
Wojciech Diechtiar <wojciech.diechtiar@gmail.com>
2020-04-05 11:13:09 +0200

    List data with *ngFor directive

---
+++ src/app/author/author.component.ts
@@ -0,0 +1,20 @@
+import { AuthorsService } from './../authors.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'author',
+  templateUrl: './author.component.html',
+  styleUrls: ['./author.component.css']
+})
+export class AuthorComponent implements OnInit {
+
+  authors;
+
+  constructor(service: AuthorsService) {
+    this.authors = service.getAuthors();
+  }
+
+  ngOnInit(): void {
+  }
+
+}
